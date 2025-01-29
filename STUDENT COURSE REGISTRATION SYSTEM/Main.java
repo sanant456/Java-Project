239e9e458173dd72504c50a7f6e71fd85e2baa56
Anant Singh <154836519+sanant456@users.noreply.github.com>
2025-01-29 19:35:12 +0530

    Update Main.java

--- STUDENT COURSE REGISTRATION SYSTEM/Main.java
+++ STUDENT COURSE REGISTRATION SYSTEM/Main.java
@@ -1,1 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        CourseDatabase courseDatabase = new CourseDatabase();
+        StudentDatabase studentDatabase = new StudentDatabase();
+        studentDatabase.addStudent(new Student("S101", "Alice"));
+        studentDatabase.addStudent(new Student("S102", "Bob"));
+        studentDatabase.addStudent(new Student("S103", "Aryan" ));
+        studentDatabase.addStudent(new Student("S104", "Sumit"));
+        studentDatabase.addStudent(new Student("S105", "Harsh"));
+        studentDatabase.addStudent(new Student("S106", "Piyush" ));
+        RegistrationSystem system = new RegistrationSystem(courseDatabase, studentDatabase);
+        system.start();
+    }
+}
 
